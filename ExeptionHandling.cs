af58a8617f6b0902b96f9ec1b2b17bcfadb879a7
Saaffe01 <125167176+Saaffe01@users.noreply.github.com>
2026-05-08 20:06:50 -0700

    Add files via upload

---
+++ ExeptionHandling.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // STRINGS AND INTEGERS SUBMISSION ASSIGNMENT
+        // List of integers. Also asking the user to divide each number in the list by.
+        var numbers = new List<int>() { 10, 25, 50, 100, 200 };
+        
+        Console.WriteLine("Pick a number to divide from the list");
+        int numbersDivided = Convert.ToInt32(Console.ReadLine());
+
+        try
+        {
+            foreach (var number in numbers)
+            {
+                Console.WriteLine(number / numbersDivided); // number selected from user is divided by 2.
+            }
+        } // catching first exception when divided by Zero 
+        catch (DivideByZeroException ex)
+        {
+            Console.WriteLine(ex.Message + "Please do not divider by zero"); // Lets user know that input cannot be 0 
+        }
+        catch (FormatException ex)
+        {
+            Console.WriteLine(ex.Message + "Please do not divide by string"); // Lets user know input cannot be a string
+        }
+        Console.WriteLine("Program has continued after the try/catch block!");
+    }
+}
