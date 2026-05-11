bf8cfb833fdc1b8a9daabf7f633272266d547e42
Saaffe01 <125167176+Saaffe01@users.noreply.github.com>
2026-05-10 22:12:22 -0700

    Add files via upload

---
+++ MathOperations.cs
@@ -0,0 +1,19 @@
+namespace MethodAssignment;
+
+public class MathOperations // Math operations class
+{
+    public int Double(int number)
+    {
+        return number * 2; 
+    }
+
+    public int TripleInt(int number)
+    {
+        return number * 3; 
+    }
+
+    public int AddNumber(int number)
+    {
+        return number + 2;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace MethodAssignment;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Please enter a number: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+        
+        MathOperations math = new MathOperations();
+        
+        Console.WriteLine(math.Double(number));
+        Console.WriteLine(math.AddNumber(number));
+        Console.WriteLine(math.TripleInt(number));
+    }
+}
