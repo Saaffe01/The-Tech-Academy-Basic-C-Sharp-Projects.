27384d532da8083962f74bc8c511e1d1fc54d678
Saaffe01 <125167176+Saaffe01@users.noreply.github.com>
2026-05-12 22:44:19 -0700

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,17 +1,28 @@
-﻿namespace MethodOverloading;
+﻿namespace MethodSubmission;
 
 class Program
 {
     static void Main(string[] args)
     {
-        var calculations = new Calculations(); // Calling calculator and displaying result
-        int result = calculations.Calculator(10);
-        Console.WriteLine(result);
+        // Prompt for user to get first number
+        Console.WriteLine("Enter the first number: ");
+        int firstNumber = int.Parse(Console.ReadLine());
         
-        int result2 = calculations.Calculator(1.23m); // Call calculator with a decimal and display result
-        Console.WriteLine(result2);
+        // Prompt for user to let them know it's optional
+        Console.WriteLine("Enter the second number: ");
+        string secondInput = Console.ReadLine();
 
-        int result3 = calculations.Calculator("20"); // Call Calculator with string and display result
-        Console.WriteLine(result3);
+        var math = new Mathematics();
+
+        // checks if they entered a 2nd number
+        if (secondInput == "")
+        {
+            Console.WriteLine(math.Calculate(firstNumber));
+        }
+        else
+        {
+            int secondNumber = int.Parse(secondInput);
+            Console.WriteLine(math.Calculate(firstNumber, secondNumber));
+        }
     }
-}
+} 
