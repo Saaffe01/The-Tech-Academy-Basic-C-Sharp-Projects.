836ba50c83a09dd229ab5f5b1395ea3190de05f6
Saaffe01 <125167176+Saaffe01@users.noreply.github.com>
2026-05-15 14:00:07 -0700

    Add files via upload

---
+++ ClassSubmission/MathHelper.cs
@@ -0,0 +1,9 @@
+namespace ClassSubmission;
+
+class MathHelper
+{
+    public void DivideByTwo(int number) // Void method that takes a numer and divides it by 2
+    {
+        Console.WriteLine(number / 2);
+    }
+}
