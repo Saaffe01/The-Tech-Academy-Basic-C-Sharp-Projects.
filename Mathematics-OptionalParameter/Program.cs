48ec015780a9aba14564109a1083bc35cac93776
Saaffe01 <125167176+Saaffe01@users.noreply.github.com>
2026-05-15 10:11:28 -0700

    Update Program.cs

--- Mathematics-OptionalParameter/Program.cs
+++ Mathematics-OptionalParameter/Program.cs
@@ -1,1 +1,11 @@
 
+namespace MethodSubmission;
+
+public class Mathematics
+{
+    public int Calculate(int firstNumber, int secondNumber = 0)
+    {
+        return firstNumber + secondNumber;
+    }
+    
+}
