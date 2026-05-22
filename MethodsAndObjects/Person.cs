db187416370c3db328c18067d503fc0d130aebdb
Saaffe01 <125167176+Saaffe01@users.noreply.github.com>
2026-05-21 21:36:59 -0700

    Enhance comments in Person class for clarity
    Updated comments to clarify the purpose of the Person class and its members.

--- MethodsAndObjects/Person.cs
+++ MethodsAndObjects/Person.cs
@@ -1,11 +1,11 @@
 namespace MethodsAndObjects;
 
-public class Person // Person class
+public class Person // Abstract class that serves as a base for the other classes
 {
-    public string FirstName { get; set; }
-    public string LastName { get; set; }
+    public string FirstName { get; set; } // Property to store the first name
+    public string LastName { get; set; } // Property to store the last name
 
-    public void SayName()
+    public void SayName() // Abstract method that must be implemented by any class that inherits Person
     {
         Console.WriteLine("Name: " + FirstName + " " + LastName);
     }
