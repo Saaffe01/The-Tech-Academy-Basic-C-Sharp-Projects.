8dc0a069ddbdb5f9bfb392d2405c081d9267abbb
Saaffe01 <125167176+Saaffe01@users.noreply.github.com>
2026-05-22 15:46:39 -0700

    Update Employee.cs

--- Polymorphism/Employee.cs
+++ Polymorphism/Employee.cs
@@ -1,8 +1,8 @@
 namespace Polymorphism;
 
-public class Employee : Person, IQuitable
+public class Employee : Person, IQuitable // Employee inherits from Person and uses IQuitable
 {
-    public void Quit()
+    public void Quit() // Uses the quit method needed for the IQuittable interface
     {
         Console.WriteLine("The employee has quit!");
     }
