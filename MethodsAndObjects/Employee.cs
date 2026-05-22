4caeb17dcb8c0176e627d49fc18c3723ac2b732c
Saaffe01 <125167176+Saaffe01@users.noreply.github.com>
2026-05-21 21:40:57 -0700

    Update Employee.cs

--- MethodsAndObjects/Employee.cs
+++ MethodsAndObjects/Employee.cs
@@ -2,5 +2,5 @@ namespace MethodsAndObjects;
 
 public class Employee : Person // created new employee class using inheritance
 {
-    public int Id { get; set; }
+    public int Id { get; set; } // property to store employee's ID
 }
