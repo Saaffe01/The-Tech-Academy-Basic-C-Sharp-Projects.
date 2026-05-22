0dadbaa7cc5af74a8c6001fc4adb5ad8e22ac2d2
Saaffe01 <125167176+Saaffe01@users.noreply.github.com>
2026-05-21 21:39:09 -0700

    Add comments to clarify employee object usage

--- MethodsAndObjects/Program.cs
+++ MethodsAndObjects/Program.cs
@@ -4,10 +4,10 @@ class Program
 {
     static void Main(string[] args)
     {
-        Employee employee = new Employee();
-        employee.FirstName = "Sample";
-        employee.LastName = "Student";
+        Employee employee = new Employee(); // Instantiate a new Employee object
+        employee.FirstName = "Sample"; // sets the first name property 
+        employee.LastName = "Student"; // sets the last name property
         
-        employee.SayName();
+        employee.SayName(); // call the SayName method inherited from Person
     }
 }
